5a38a011acdfc8ff33dc0478f165d67ac150323e
Cristian Morataya <cris.morataya@gmail.com>
2016-09-30 14:11:44 -0600

    Remap the movement keys, and the clockwise rotation

--- scripts/kinematic.gd
+++ scripts/kinematic.gd
@@ -2,19 +2,24 @@
 extends KinematicBody2D
 
 const speed = 400
+const radian = 0.1
 
 func _ready():
 	set_fixed_process(true)
 
 func _fixed_process(delta):
 	var direction = Vector2(0,0)
-	if ( Input.is_action_pressed("ui_up") ):
+	if ( Input.is_action_pressed("player_up") ):
 		direction += Vector2(0,-1)
-	if ( Input.is_action_pressed("ui_down") ):
+		rotate(-radian)
+	if ( Input.is_action_pressed("player_down") ):
 		direction += Vector2(0,1)
-	if ( Input.is_action_pressed("ui_left") ):
+		rotate(-radian)
+	if ( Input.is_action_pressed("player_left") ):
 		direction += Vector2(-1,0)
-	if ( Input.is_action_pressed("ui_right") ):
+		rotate(-radian)
+	if ( Input.is_action_pressed("player_right") ):
 		direction += Vector2(1,0)
+		rotate(-radian)
 	
 	move(direction*speed*delta)
