c0ec903df256b1d7802e05e76d5c8753b3e2ae6d
Cristian Morataya <cris.morataya@gmail.com>
2016-10-10 23:26:26 -0600

    Change the icons, walls and add text

--- scripts/Sprite.gd
+++ scripts/Sprite.gd
@@ -10,17 +10,17 @@ func _ready():
 func _fixed_process(delta):
 	var nodo = get_node("KinematicBody2D")
 	var direction = Vector2(0,0)
-	if ( get_node("up").is_pressed() ):
+	if ( get_node("up").is_pressed() || Input.is_action_pressed("ui_up") ):
 		print("Up presed")
 		direction += Vector2(0,-1)
 		nodo.rotate(radian)
-	if ( get_node("down").is_pressed() ):
+	if ( get_node("down").is_pressed() || Input.is_action_pressed("ui_down")):
 		direction += Vector2(0,1)
 		nodo.rotate(-radian)
-	if ( get_node("left").is_pressed() ):
+	if ( get_node("left").is_pressed() || Input.is_action_pressed("ui_left")):
 		direction += Vector2(-1,0)
 		nodo.rotate(radian)
-	if ( get_node("right").is_pressed() ):
+	if ( get_node("right").is_pressed() || Input.is_action_pressed("ui_right")):
 		direction += Vector2(1,0)
 		nodo.rotate(-radian)
 	
